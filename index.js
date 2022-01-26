ef5eae8c7618313f8e8976adcbf6c065a0c67a46
Abdul Rauf <abdulraufmujahid@gmail.com>
2022-01-26 14:45:48 +0500

    convert to commonjs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import KcAdminClient from '@keycloak/keycloak-admin-client';
+const KcAdminClient =  require('@keycloak/keycloak-admin-client').default;
 
 // To configure the client, pass an object to override any of these  options:
 // {
@@ -8,32 +8,39 @@ import KcAdminClient from '@keycloak/keycloak-admin-client';
 //     /* Axios request config options https://github.com/axios/axios#request-config */
 //   },
 // }
-const kcAdminClient = new KcAdminClient();
+async function main() {
+  const kcAdminClient = new KcAdminClient();
 
-// Authorize with username / password
-await kcAdminClient.auth({
-  username: 'wwwy3y3',
-  password: 'wwwy3y3',
-  grantType: 'password',
-  clientId: 'admin-cli',
-  totp: '123456', // optional Time-based One-time Password if OTP is required in authentication flow
-});
+  // Authorize with username / password
+  await kcAdminClient.auth({
+    username: 'wwwy3y3',
+    password: 'wwwy3y3',
+    grantType: 'password',
+    clientId: 'admin-cli',
+    totp: '123456', // optional Time-based One-time Password if OTP is required in authentication flow
+  });
+  
+  // List all users
+  const users = await kcAdminClient.users.find();
+  
+  // Override client configuration for all further requests:
+  kcAdminClient.setConfig({
+    realmName: 'another-realm',
+  });
+  
+  // This operation will now be performed in 'another-realm' if the user has access.
+  const groups = await kcAdminClient.groups.find();
+  
+  // Set a `realm` property to override the realm for only a single operation.
+  // For example, creating a user in another realm:
+  await this.kcAdminClient.users.create({
+    realm: 'a-third-realm',
+    username: 'username',
+    email: 'user@example.com',
+  });
+}
 
-// List all users
-const users = await kcAdminClient.users.find();
 
-// Override client configuration for all further requests:
-kcAdminClient.setConfig({
-  realmName: 'another-realm',
-});
-
-// This operation will now be performed in 'another-realm' if the user has access.
-const groups = await kcAdminClient.groups.find();
-
-// Set a `realm` property to override the realm for only a single operation.
-// For example, creating a user in another realm:
-await this.kcAdminClient.users.create({
-  realm: 'a-third-realm',
-  username: 'username',
-  email: 'user@example.com',
-});
+main()
+  .then(console.log)
+  .catch(console.error)
